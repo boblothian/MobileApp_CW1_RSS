434c0720b6c779a3d14db0dd71313eb66dc32f93
boblothian <robertmlothian@gmail.com>
2024-02-29 18:06:12 +0000

    RSS Parsing working for 1 day

--- app/src/main/java/com/example/lothian_robert_rlothi300/MainActivity.java
+++ app/src/main/java/com/example/lothian_robert_rlothi300/MainActivity.java
@@ -1,20 +1,17 @@
-// Name                 Robert Lothian
-// Student ID           RLOTHI300
-// Programme of Study   Computer Science
-
 package com.example.lothian_robert_rlothi300;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,134 +19,212 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener
-{
+public class MainActivity extends AppCompatActivity implements OnClickListener {
     private TextView rawDataDisplay;
+    private TextView weatherDataDisplay;
     private Button startButton;
-    private String result;
-    private String url1="";
-    private String urlSource="https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123";
+    private String urlSource = "https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/2643123";
+
+    public WeatherInfo weatherInfo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Set up the raw links to the graphical components
-        rawDataDisplay = (TextView)findViewById(R.id.rawDataDisplay);
-        startButton = (Button)findViewById(R.id.startButton);
+        rawDataDisplay = findViewById(R.id.rawDataDisplay);
+        weatherDataDisplay = findViewById(R.id.weatherData);
+        startButton = findViewById(R.id.startButton);
         startButton.setOnClickListener(this);
 
         // More Code goes here
     }
 
-    public void onClick(View aview)
-    {
+    public void onClick(View aview) {
         startProgress();
     }
 
-    public void startProgress()
-    {
+    public void startProgress() {
         // Run network access on a separate thread;
         new Thread(new Task(urlSource)).start();
-    } //
+    }
 
     // Need separate thread to access the internet resource over network
     // Other neater solutions should be adopted in later iterations.
-    private class Task implements Runnable
-    {
+    private class Task implements Runnable {
         private String url;
 
-        public Task(String aurl)
-        {
+        public Task(String aurl) {
             url = aurl;
         }
-        @Override
-        public void run()
-        {
 
+        @Override
+        public void run() {
             URL aurl;
             URLConnection yc;
             BufferedReader in = null;
             String inputLine = "";
-
+            String result = ""; // Initialize result
 
             Log.e("MyTag","in run");
 
-            try
-            {
+            try {
                 Log.e("MyTag","in try");
                 aurl = new URL(url);
                 yc = aurl.openConnection();
                 in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-                while ((inputLine = in.readLine()) != null)
-                {
+                while ((inputLine = in.readLine()) != null) {
                     result = result + inputLine;
                     Log.e("MyTag",inputLine);
-
                 }
                 in.close();
-            }
-            catch (IOException ae)
-            {
+            } catch (IOException ae) {
                 Log.e("MyTag", "ioexception");
             }
 
-            //Get rid of the first tag <?xml version="1.0" encoding="utf-8"?>
-            int i = result.indexOf(">");
-            result = result.substring(i+1);
-            //Get rid of the 2nd tag <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-            i = result.indexOf(">");
-            result = result.substring(i+1);
-            Log.e("MyTag - cleaned",result);
+            Log.e("MyTag","Xml Data"+ result);
 
             parseData(result);
-            //
-            // Now that you have the xml data you can parse it
-            //
-
-
-            // Now update the TextView to display raw XML data
-            // Probably not the best way to update TextView
-            // but we are just getting started !
-
-
-            MainActivity.this.runOnUiThread(new Runnable()
-            {
-
-                public void run() {
-
-                    Log.d("UI thread", "I am the UI thread");
-                    rawDataDisplay.setText(result);
-                }
-            });
         }
-
-        private void parseData(String result) {
-            try {
-                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-                    factory.setNamespaceAware(true);
-                    XmlPullParser xpp = factory.newPullParser();
-                    xpp.setInput(new StringReader(result));
-                    int eventType = xpp.getEventType();
-                    while (eventType != XmlPullParser.END_DOCUMENT)
-                    {
-                        if(eventType == XmlPullParser.START_TAG)
-                        {
-                            if (xpp.getName().equalsIgnoreCase(""))
-                            {
-                                Log.d("MyTag", "");
-                            }
-                        }
-                    }
-            }
-            catch (XmlPullParserException ael)
-            {
-                Log.e("Parsing error" + ael.toString());
-            }
-        }
-
     }
 
+    private void parseData(String dataToParse) {
+        List<WeatherInfo> weatherInfoList = new ArrayList<>();
+
+
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(new StringReader(dataToParse));
+
+            int eventType = parser.getEventType();
+
+            String tagName = null;
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        tagName = parser.getName();
+                        if (tagName.equalsIgnoreCase("item")) {
+                            weatherInfo = new WeatherInfo();
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        String text = parser.getText();
+                        if (tagName != null && weatherInfo != null) {
+                            switch (tagName) {
+                                case "description":
+                                    parseDescription(text, weatherInfo);
+                                    break;
+                            }
+                        }
+                        break;
+                }
+                eventType = parser.next();
+            }
+
+
+            if (weatherInfo != null) {
+                weatherInfoList.add(weatherInfo);
+            }
+
+            // Update UI with parsed weather information
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // Update UI components here
+                    displayWeatherInfo(weatherInfoList);
+                }
+            });
+
+        } catch (XmlPullParserException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseDescription(String description, WeatherInfo weatherInfo){
+        Log.e("Description", description);
+        String[] parts = description.split(", ");
+
+        for (String part : parts) {
+            String[] keyValue = part.split(": ");
+            if (keyValue.length ==2) {
+                String key = keyValue[0];
+                String value = keyValue[1];
+
+                switch (key) {
+                    case "Maximum Temperature":
+                        weatherInfo.setMaxTemperature(value);
+                        break;
+                    case "Minimum Temperature":
+                        weatherInfo.setMinTemperature(value);
+                        break;
+                    case "Wind Direction":
+                        weatherInfo.setWindDirection(value);
+                        break;
+                    case "Wind Speed":
+                        weatherInfo.setWindSpeed(value);
+                        break;
+                    case "Visibility":
+                        weatherInfo.setVisibility(value);
+                        break;
+                    case "Pressure":
+                        weatherInfo.setPressure(value);
+                        break;
+                    case "Humidity":
+                        weatherInfo.setHumidity(value);
+                        break;
+                    case "UV Risk":
+                        weatherInfo.setUvRisk(value);
+                        break;
+                    case "Pollution":
+                        weatherInfo.setPollution(value);
+                        break;
+                    case "Sunrise":
+                        weatherInfo.setSunrise(value);
+                        break;
+                    case "Sunset":
+                        weatherInfo.setSunset(value);
+                        break;
+                    default:
+                        // Handle unknown key or skip
+                        break;
+                }
+            }
+        }
+    }
+
+    private void displayWeatherInfo(List<WeatherInfo> weatherInfoList) {
+        StringBuilder weatherData = new StringBuilder();
+
+        // Append weather information for each WeatherInfo object in the list
+        for (WeatherInfo weatherInfo : weatherInfoList) {
+            // Example: Append max and min temperature
+            Log.d("WeatherInfo", "Max Temperature: " + weatherInfo.getMaxTemperature());
+            weatherData.append("Max Temperature: ").append(weatherInfo.getMaxTemperature()).append("\n");
+            weatherData.append("Min Temperature: ").append(weatherInfo.getMinTemperature()).append("\n");
+            weatherData.append("Wind Direction: ").append(weatherInfo.getWindDirection()).append("\n");
+            weatherData.append("Wind Speed: ").append(weatherInfo.getWindSpeed()).append("\n");
+            weatherData.append("Visibility: ").append(weatherInfo.getVisibility()).append("\n");
+            weatherData.append("Pressure: ").append(weatherInfo.getPressure()).append("\n");
+            weatherData.append("Humidity: ").append(weatherInfo.getHumidity()).append("\n");
+            weatherData.append("UV Risk: ").append(weatherInfo.getUvRisk()).append("\n");
+            weatherData.append("Pollution: ").append(weatherInfo.getPollution()).append("\n");
+            weatherData.append("Sunrise: ").append(weatherInfo.getSunrise()).append("\n");
+            weatherData.append("Sunset: ").append(weatherInfo.getSunset()).append("\n");
+            // Append other weather information as needed
+        }
+
+        // Update the weatherDataDisplay TextView with the formatted weather data
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                weatherDataDisplay.setText(weatherData.toString());
+            }
+        });
+    }
 }
---
+++ app/src/main/java/com/example/lothian_robert_rlothi300/WeatherInfo.java
@@ -0,0 +1,128 @@
+package com.example.lothian_robert_rlothi300;
+
+import android.util.Log;
+
+public class WeatherInfo {
+    private String maxTemperature;
+    private String minTemperature;
+    private String windDirection;
+    private String windSpeed;
+    private String visibility;
+    private String pressure;
+    private String humidity;
+    private String uvRisk;
+    private String pollution;
+    private String sunrise;
+    private String sunset;
+
+    // Constructor
+    public WeatherInfo() {
+    }
+
+    // Getters and Setters
+    public String getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(String maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public String getMinTemperature() {
+        return minTemperature;
+    }
+
+    public void setMinTemperature(String minTemperature) {
+        this.minTemperature = minTemperature;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    public String getUvRisk() {
+        return uvRisk;
+    }
+
+    public void setUvRisk(String uvRisk) {
+        this.uvRisk = uvRisk;
+    }
+
+    public String getPollution() {
+        return pollution;
+    }
+
+    public void setPollution(String pollution) {
+        this.pollution = pollution;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
+        this.sunset = sunset;
+    }
+
+    // toString method to print the object
+    @Override
+    public String toString() {
+        return "WeatherInfo{" +
+                "maxTemperature='" + maxTemperature + '\'' +
+                ", minTemperature='" + minTemperature + '\'' +
+                ", windDirection='" + windDirection + '\'' +
+                ", windSpeed='" + windSpeed + '\'' +
+                ", visibility='" + visibility + '\'' +
+                ", pressure='" + pressure + '\'' +
+                ", humidity='" + humidity + '\'' +
+                ", uvRisk='" + uvRisk + '\'' +
+                ", pollution='" + pollution + '\'' +
+                ", sunrise='" + sunrise + '\'' +
+                ", sunset='" + sunset + '\'' +
+                '}';
+    }
+}
